f1de325400d3514432479386b33e8caf6c4f39d5
chan <ych21c@naver.com>
2026-08-22 15:56:53 +0900

    chore: app_identifier를 com.myownchild.recoveryfit으로 동기화
    배포 설정 카드에서 입력한 값을 deploy_runner가 iOS/Android/fastlane 설정에
    실제로 반영한 결과.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.recoveryfit.recovery_fit"
+        applicationId = "com.myownchild.recoveryfit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
